4a4081e3f2da731c171d659e4306fe39f82ed459
kaua <letesk@gmail.com>
2023-03-13 08:22:31 -0300

    add e modificação css

---
+++ java.js
@@ -0,0 +1,10 @@
+
+    let res=document.getElementById('res')
+    let none_cidade = []
+
+    function inserir(){
+        let cidade = document.getElementById('cidade').value
+        console.log(cidade)
+        none_cidade.push(cidade)
+        console.log(none_cidade)
+    }
